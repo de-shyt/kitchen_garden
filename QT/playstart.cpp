5eeb1e4c0f07cceb161d849b4607423d281cf8ab
metalcoffee <75562690+metalcoffee@users.noreply.github.com>
2022-03-06 11:32:22 +0300

    Update playstart.cpp

--- QT/playstart.cpp
+++ QT/playstart.cpp
@@ -8,25 +8,19 @@
 #include <QFont>
 PlayStart::PlayStart(QWidget *parent)
 {
-
-   // setWindowTitle("POTATO SIMULATOR");
-    //setStyleSheet(("background-image: url(:/img/weed.jpg)"));
     QGraphicsScene * scene = new QGraphicsScene();
-    // create an item to add to the scene
+   //create a FarMER
     player * my_player = new player();
-    my_player->setPixmap(QPixmap(":/img/top_farmer.png")); // change the rect from 0x0 (default) to 100x100 pixels
+    my_player->setPixmap(QPixmap(":/img/top_farmer.png")); 
     scene->setBackgroundBrush(QBrush(QImage(":/img/weed.jpg")));
-    // add the item to the scene
+    // add FARMER to the ferma
     scene->addItem(my_player);
-
-    // make rect focusable
+   
     my_player->setFlag(QGraphicsItem::ItemIsFocusable);
     my_player->setFocus();
-
-    // create a view to visualize the scene
+   
     QGraphicsView * view = new QGraphicsView(scene);
 
-    // show the view
     view->show();
 
 }
