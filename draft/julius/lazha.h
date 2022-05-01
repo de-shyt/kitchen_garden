fd8d08b33154e4567f95216b0ca3870125370371
JuliaKud <90181418+JuliaKud@users.noreply.github.com>
2022-05-01 16:46:46 +0300

    Create lazha.h

---
+++ draft/julius/lazha.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+const int HEIGHT_MAP = 5;
+const int WIDTH_MAP = 5;
+
+//sf::String TileMap[HEIGHT_MAP] = {
+//        "00000",
+//        "00000",
+//        "00000",
+//        "00000",
+//        "00000",
+//};
