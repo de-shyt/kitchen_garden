b8899242191bd2d95c7baca1e917d034e1c0b333
de-shyt <90146924+de-shyt@users.noreply.github.com>
2022-05-01 16:43:52 +0300

    Add files via upload

---
+++ draft/deshur/Player.h
@@ -0,0 +1,42 @@
+#ifndef DRAFT_PLAYER_H
+#define DRAFT_PLAYER_H
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+enum class Direction {Left, Right, Up, Down};
+
+
+namespace custom_exceptions {
+    struct unable_to_open_a_file : std::runtime_error {
+        explicit unable_to_open_a_file(const std::string& name);
+    };
+}
+
+
+struct BaseElem {
+    float x = 0, y = 0, w = 0, h = 0;
+    std::string FileName;
+    sf::Sprite mSprite;
+    sf::Texture mTexture;
+    sf::Image mImage;
+
+    BaseElem();
+    explicit BaseElem(float x_, float y_, float w_, float h_, std::string&& name);
+//    BaseElem& operator=(BaseElem&& Elem) noexcept;
+};
+
+
+
+struct Player : BaseElem {
+    float dx = 0, dy = 0, speed = 0;
+    Direction dir = Direction::Right;
+
+    Player();
+    explicit Player(float x_, float y_, float w_, float h_, std::string&& name);
+    void update(float time);
+};
+
+
+
+#endif //DRAFT_PLAYER_H
