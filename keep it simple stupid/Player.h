3ea17e7e7f6c28e41f18c84e29125a12964b084f
de-shyt <90146924+de-shyt@users.noreply.github.com>
2022-05-03 18:46:51 +0300

    Update Player.h

--- keep it simple stupid/Player.h
+++ keep it simple stupid/Player.h
@@ -2,6 +2,7 @@
 #define DRAFT_PLAYER_H
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 enum class Direction {Left, Right, Up, Down};
@@ -42,5 +43,54 @@ struct Player : BaseElem {
 };
 
 
+
+struct BaseStruct {
+    virtual std::string CheckBoundaries(sf::Vector2i& MousePos) const = 0;
+};
+
+
+struct Menu : BaseStruct {
+    BaseElem NewGame = BaseElem(100, 30, 169, 39, "newgame.png");
+    BaseElem Exit = BaseElem(100, 90, 110, 41, "exit.png");
+    BaseElem Carrot = BaseElem(300, -20, 918, 950, "carrot.png");
+//    Carrot.mSprite.setRotation(15);
+
+    void Draw(sf::RenderWindow& window) const;
+    void ChangeColor(sf::RenderWindow& window);
+    std::string CheckBoundaries(sf::Vector2i& MousePos) const;
+};
+
+
+struct Map : BaseStruct {
+    BaseElem BG = BaseElem(0, 0, 2048, 2048, "grassBG.png");
+    BaseElem Shop = BaseElem(50, 50, 100, 100, "shop.png");
+    BaseElem Chat = BaseElem(50, 200, 100, 100, "chat.png");
+    BaseElem Pause = BaseElem(1770, 50, 100, 100, "pause.png");
+
+    std::string CheckBoundaries(sf::Vector2i& MousePos) const;
+    void Draw(sf::RenderWindow& window);
+};
+
+
+struct Shop : BaseStruct {
+    BaseElem BG = BaseElem(0, 0, 2048, 2048, "shopBG.png");
+    BaseElem Close = BaseElem(1770, 50, 100, 100, "close.png");
+    std::vector<BaseElem> Items;
+
+    Shop();
+    ~Shop();
+    void Draw(sf::RenderWindow& window) const;
+    std::string CheckBoundaries(sf::Vector2i& MousePos) const;
+};
+
+
+struct Chat : BaseStruct {
+    BaseElem Close = BaseElem(1770, 50, 100, 100, "close.png");
+
+    Chat();
+    ~Chat();
+    void Draw(sf::RenderWindow& window) const;
+    std::string CheckBoundaries(sf::Vector2i& MousePos) const;
+};
 
 #endif //DRAFT_PLAYER_H
