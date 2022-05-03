43b7c40ab7f1ba89d815cb7949023d33dc81daa3
de-shyt <90146924+de-shyt@users.noreply.github.com>
2022-05-03 18:47:39 +0300

    Update Player.cpp

--- keep it simple stupid/Player.cpp
+++ keep it simple stupid/Player.cpp
@@ -1,8 +1,7 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <fstream>
-#include <string>
 #include "Player.h"
+//#define FILE_LOCATION "C:/Users/NB/CLionProjects/draft1/images/"
+#define FILE_LOCATION "images/"
+
 
 custom_exceptions::unable_to_open_a_file::unable_to_open_a_file(const std::string& name)
         : std::runtime_error("Unable to open file '" + name + "'\n") {}
@@ -11,15 +10,16 @@ custom_exceptions::unable_to_open_a_file::unable_to_open_a_file(const std::strin
 
 BaseElem::BaseElem() = default;
 
+
 BaseElem::BaseElem(float x_, float y_, float w_, float h_, std::string &&name)
     : x(x_), y(y_), w(w_), h(h_), FileName(std::move(name))
 {
-    std::ifstream f("images/" + FileName);
+    std::ifstream f(FILE_LOCATION + FileName);
     if (!f.is_open()) {
         throw custom_exceptions::unable_to_open_a_file(FileName);
     }
 
-    mImage.loadFromFile("images/" + FileName);
+    mImage.loadFromFile(FILE_LOCATION + FileName);
     mTexture.loadFromImage(mImage);
     mSprite.setTexture(mTexture);
     mSprite.setPosition(x, y);
@@ -30,13 +30,14 @@ BaseElem::BaseElem(float x_, float y_, float w_, float h_, std::string &&name)
 
 Player::Player() = default;
 
+
 Player::Player(float x_, float y_, float w_, float h_,
                float recTop, float recLeft, std::string &&name)
-    : BaseElem(x_, y_, w_, h_, std::move(name)) {
-//    mImage.createMaskFromColor(sf::Color(0,0,0));
-//    mImage.createMaskFromColor(sf::Color(255,255,255));
+    : BaseElem(x_, y_, w_, h_, std::move(name))
+{
     mSprite.setTextureRect(sf::IntRect(recLeft, recTop, w, h));
 }
+
 
 void Player::update(float time) {
     switch(dir) {
@@ -60,6 +61,7 @@ void Player::update(float time) {
     mSprite.setPosition(x, y);
 }
 
+
 void Player::GoLeft(float &CurrentFrame, float &time) {
     dir = Direction::Left;
     speed = 0.25;
@@ -67,6 +69,7 @@ void Player::GoLeft(float &CurrentFrame, float &time) {
     if (CurrentFrame > 4) CurrentFrame -= 4;
     mSprite.setTextureRect(sf::IntRect(64 * int(CurrentFrame), 96, 64, 96));
 }
+
 
 void Player::GoRight(float &CurrentFrame, float &time) {
     dir = Direction::Right;
@@ -76,6 +79,7 @@ void Player::GoRight(float &CurrentFrame, float &time) {
     mSprite.setTextureRect(sf::IntRect(64 * int(CurrentFrame), 192, 64, 96));
 }
 
+
 void Player::GoUp(float &CurrentFrame, float &time) {
     dir = Direction::Up;
     speed = 0.25;
@@ -83,11 +87,12 @@ void Player::GoUp(float &CurrentFrame, float &time) {
     if (CurrentFrame > 4) CurrentFrame -= 4;
     mSprite.setTextureRect(sf::IntRect(64 * int(CurrentFrame), 288, 64, 96));
 }
+
+
 void Player::GoDown(float &CurrentFrame, float &time) {
-    dir = Direction::Down; speed = 0.25;
+    dir = Direction::Down;
+    speed = 0.25;
     CurrentFrame += 0.005 * time;
     if (CurrentFrame > 4) CurrentFrame -= 4;
     mSprite.setTextureRect(sf::IntRect(64 * int(CurrentFrame), 0, 64, 96));
 }
-
-
