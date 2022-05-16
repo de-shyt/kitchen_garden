a58080bf3e378e695401ee452bda4ef637b49fdd
de-shyt <de.shut@inbox.ru>
2022-05-17 00:30:51 +0300

    please, fix tr

--- keep it simple stupid/Menu.cpp
+++ keep it simple stupid/Menu.cpp
@@ -67,6 +67,7 @@ void Menu::ChangeColor(sf::RenderWindow &window)
 std::string Menu::CheckBoundaries(sf::Vector2i& MousePos) {
     if (NewGame.mText.getGlobalBounds().contains(MousePos.x, MousePos.y)) {
         MapPtr->BoughtItems.clear();
+        soci::transaction tr(sql);
         sql << "delete from players";
         sql << "delete from objects_on_map";
         sql << "insert into players values ('abober')";
