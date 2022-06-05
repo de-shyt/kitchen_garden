5fe3d7c02ce5bcfb234f947658f1bac368a8ed16
de-shyt <90146924+de-shyt@users.noreply.github.com>
2022-06-05 12:01:24 +0300

    Update BaseElem.cpp

--- keep it simple stupid/BaseElem.cpp
+++ keep it simple stupid/BaseElem.cpp
@@ -1,9 +1,11 @@
 #include "BaseElem.h"
 #include "BaseStruct.h"
+#include <cmath>
+
 
 
 custom_exceptions::unable_to_open_a_file::unable_to_open_a_file(const std::string& name)
-    : std::runtime_error("Unable to open file '" + name + "'\n") {}
+        : std::runtime_error("Unable to open file '" + name + "'\n") {}
 
 
 
@@ -13,7 +15,7 @@ BaseElem::BaseElem() = default;
 
 
 BaseElem::BaseElem(float x_, float y_, float w_, float h_, std::string &&name) :
-    x(x_), y(y_), w(w_), h(h_), FileName(std::move(name))
+        x(x_), y(y_), w(w_), h(h_), FileName(std::move(name))
 {
     std::ifstream f("images/" + FileName);
     if (!f.is_open()) {
@@ -36,8 +38,8 @@ MapElem::MapElem() = default;
 
 
 MapElem::MapElem(float x_, float y_, float w_, float h_, std::string&& name, std::string&& type_id_, int id_)
-    : BaseElem(x_, y_, w_, h_, std::move(name)),
-      type_id(std::move(type_id_)), id(id_) {}
+        : BaseElem(x_, y_, w_, h_, std::move(name)),
+          type_id(std::move(type_id_)), id(id_) {}
 
 
 MapElem::MapElem(float x_, float y_, float w_, float h_, std::string&& name, std::string& type_id_, int id_)
@@ -53,20 +55,121 @@ GardenBed::GardenBed() : product(nullptr) {}
 
 GardenBed::GardenBed(float x_, float y_, float w_, float h_, std::string&& name, int id_)
         : BaseElem(x_, y_, w_, h_, std::move(name)),
-          id(id_), product(nullptr) {}
+          id(id_), product(nullptr), cur_fragment(0) {}
 
 
-void GardenBed::CheckProductCoords()
+void GardenBed::ModifyProduct(soci::session& sql)
 {
     if (product == nullptr) {
         return;
     }
+
+    std::string time_start, time_finish, segment;
+    sql << "select time_start, time_finish, segment from garden_bed_products where gar_bed_id=(:id)",
+            soci::use(id), soci::into(time_start), soci::into(time_finish), soci::into(segment);
+
+    std::string cur_time;
+    sql << "select current_timestamp", soci::into(cur_time);
+
+    int statement;
+    sql << "select (:cur_time)::timestamp >= (:time_finish)::timestamp", soci::use(cur_time), soci::use(time_finish), soci::into(statement);
+
+    if (statement) {
+        if (cur_fragment == 5) {
+            cur_fragment = 6;
+            soci::transaction tr(sql);
+            sql << "update garden_bed_products set cur_fragment = 6 where gar_bed_id=(:id)", soci::use(id);
+            tr.commit();
+        }
+        else if (cur_fragment == 4) {
+            cur_fragment = 5;
+            time_start = cur_time;
+            std::string type_id = product->type_id;
+            sql << "select time_to_rot from time_defaults where type_id=(:type_id)", soci::use(type_id), soci::into(time_finish);
+            sql << "select (:time_start)::timestamp + (:time_finish)::interval", soci::use(time_start), soci::use(time_finish), soci::into(time_finish);
+            soci::transaction tr(sql);
+            sql << "update garden_bed_products set time_start=(:time_start)::timestamp, time_finish=(:time_finish)::timestamp, cur_fragment = 5 where gar_bed_id=(:id)",
+                    soci::use(time_start), soci::use(time_finish), soci::use(id);
+            tr.commit();
+        }
+    }
+
+    sql << "select (:time_start)::timestamp + (:segment)::interval * (:cur_fragment + 1) < (:cur_time)::timestamp",
+            soci::use(time_start), soci::use(segment), soci::use(cur_fragment), soci::use(cur_time), soci::into(statement);
+
+    if (statement) {
+        cur_fragment = std::min(cur_fragment + 1, 5);
+        sql << "update garden_bed_products set cur_fragment=(:cur_fragment) where gar_bed_id=(:id)",
+                soci::use(cur_fragment), soci::use(id);
+    }
+
+    product->mSprite.setTextureRect(sf::IntRect(cur_fragment * 50, 0, 50, 110));
+
     product->x = x;
-    product->y = y + h - product->h;
+    product->y = y - product->h;
     product->mSprite.setPosition(product->x, product->y);
 }
 
 
+int GardenBed::dist(int other_x, int other_y) {
+    return std::sqrt((x - other_x) * (x - other_x) + (y - other_y) * (y - other_y));
+}
+
+
+std::string GardenBed::WishToHarvestCrop(int player_x, int player_y, Map* MapPtr)
+{
+    if (dist(player_x, player_y) > 200) {
+        return "Map";
+    }
+
+    if (product == nullptr) {
+        MapPtr->ShopPtr->CurrentMode = Mode::BuyGardenBedPlant;
+        MapPtr->ShopPtr->GardenBedPtr = this;
+        return "Shop";
+    }
+
+    if (cur_fragment < 5) {
+        return "Map";
+    }
+
+    if (cur_fragment == 5) { // plant is ripen
+        std::string type_id = product->type_id;
+        int profit;
+        MapPtr->ShopPtr->sql << "select cost_sell from cost_defaults where type_id=(:type_id)", soci::use(type_id), soci::into(profit);
+        MapPtr->Money.balance += profit;
+
+        int balance = MapPtr->Money.balance;
+        std::string name = MapPtr->PlayerPtr->name;
+        soci::transaction tr(MapPtr->sql);
+        MapPtr->sql << "update players set money=(:balance) where name=(:name)", soci::use(balance), soci::use(name);
+        tr.commit();
+    }
+
+    soci::transaction tr(MapPtr->sql);
+    MapPtr->sql << "delete from garden_bed_products where gar_bed_id=(:id)", soci::use(id);
+    tr.commit();
+
+    product->mSprite.setColor(sf::Color::Transparent);
+    product = nullptr;
+    cur_fragment = 0;
+
+    return "Map";
+}
+
+void GardenBed::Draw(sf::RenderWindow &window, soci::session& sql)
+{
+    window.draw(mSprite);
+
+    if (product != nullptr)
+    {
+        product->mSprite.setColor(sf::Color::White);
+        if (cur_fragment == 5) {
+            product->mSprite.setColor(sf::Color::Yellow);
+        }
+
+        window.draw(product->mSprite);
+    }
+}
 
 
 // ============================================= GardenBedElem =====================================================
@@ -75,13 +178,13 @@ GardenBedElem::GardenBedElem() = default;
 
 
 GardenBedElem::GardenBedElem(float x_, float y_, float w_, float h_, std::string&& name, std::string&& type_id_, int id_)
-    : BaseElem(x_, y_, w_, h_, std::move(name)),
-      type_id(std::move(type_id_)), id(id_) {}
+        : BaseElem(x_, y_, w_, h_, std::move(name)),
+          type_id(std::move(type_id_)), id(id_) {}
 
 
 GardenBedElem::GardenBedElem(float x_, float y_, float w_, float h_, std::string&& name, std::string& type_id_, int id_)
-    : BaseElem(x_, y_, w_, h_, std::move(name)),
-      type_id(type_id_), id(id_) {}
+        : BaseElem(x_, y_, w_, h_, std::move(name)),
+          type_id(type_id_), id(id_) {}
 
 
 
@@ -90,9 +193,8 @@ GardenBedElem::GardenBedElem(float x_, float y_, float w_, float h_, std::string
 Player::Player() = default;
 
 
-Player::Player(float x_, float y_, float w_, float h_, float recTop, float recLeft,
-               std::string&& file_name, std::string& player_name) :
-    BaseElem(x_, y_, w_, h_, std::move(file_name)), name(player_name)
+Player::Player(float x_, float y_, float w_, float h_, float recTop, float recLeft, std::string&& file_name, std::string& player_name) :
+        BaseElem(x_, y_, w_, h_, std::move(file_name)), name(player_name)
 {
     mSprite.setTextureRect(sf::IntRect(recLeft, recTop, w, h));
 }
@@ -120,6 +222,26 @@ void Player::InteractionWithMap(Map* MapPtr)
             }
         }
     }
+
+    for (auto& elem : MapPtr->GardenBeds) {
+        if (elem->mSprite.getGlobalBounds().intersects(sf::Rect(sf::Rect(x, y, w, h)))) {
+            switch(dir) {
+                case Direction::Right: {
+                    x = elem->x - w;
+                } break;
+                case Direction::Left: {
+                    x = elem->x + elem->w;
+                } break;
+                case Direction::Up: {
+                    y = elem->y + elem->h;
+                } break;
+                case Direction::Down: {
+                    y = elem->y - h;
+                } break;
+            }
+        }
+    }
+
     if (x < 0) x = 0;
     else if (x > 1920 - w) x = 1920 - w;
 
