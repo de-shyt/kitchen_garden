6fd71b9584ba3864f74e0f7c077371dbc4fd36b4
de-shyt <90146924+de-shyt@users.noreply.github.com>
2022-06-07 14:49:58 +0300

    Delete IM_LITERALLY_DEAD directory

--- IM_LITERALLY_DEAD/client/Player.cpp
+++
@@ -1,9 +0,0 @@
-#include "Player.h"
-
-
-Player::Player() : m_health(100), m_id(-1) {
-	pTexture.loadFromFile("C:/Users/mi/labs_cpp/networking_sfml/Client/Media/player.png");
-	this->setTexture(pTexture);
-}
-
-Player::~Player() {}
--- IM_LITERALLY_DEAD/server/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "Server.h"
-
-int main() {
-	Server server;
-	server.run();
-}
